eda2a797fa7b560faffd0e7d32ea7f4bc93c21f0
anmg <netamg_2008@126.com>
2018-07-03 11:36:26 +0800

    add jumpfloor and get by index

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,5 +1,42 @@
+# -*- coding: utf-8  -*-
+def Fabonacci(number):
+    a=0
+    b=1
+    print a
+    print b
 
-def Fibonacci(number):
+    while(b<number):
+        temp = a
+        a = b
+        b = b+temp
+        print b
+Fabonacci(5)
+
+def jumpfloor(number):
+    if number == 0:
+        return  0
+    if number == 1:
+        return  1
+    if number == 2:
+        return 2
+
+    a = 1
+    b = 2
+    while number>2:
+        temp = a
+        a = b
+        b = b + temp
+        number = number -1
+
+    return b
+
+# print jumpfloor(1)
+# print jumpfloor(2)
+# print jumpfloor(3)
+# print jumpfloor(4)
+# print jumpfloor(5)
+
+def get_Fibonacci_byidx(number):
     if number == 0:
         return 0
     if number == 1:
@@ -18,4 +55,4 @@ def Fibonacci(number):
         return arr[number]
 
 
-print Fibonacci(10)
+print get_Fibonacci_byIdx(10)
